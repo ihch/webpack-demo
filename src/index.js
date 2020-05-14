08acbf8f54ddbee97ee6b24d8d8ca3f3e7e08809
nemusou <ihachihiro@team-lab.com>
2020-05-14 18:19:26 +0900

    add files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// const component = () => {
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = 'Hello webpack!';
+
+  return element;
+}
+
+document.body.appendChild(component());
